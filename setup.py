c90036a7fd444f1a523255fd6ee004fbe91ed29c
levi <levi.kingma@gmail.com>
2024-05-23 20:51:09 +0200

    Added setup.py for build

---
+++ setup.py
@@ -0,0 +1,25 @@
+import sys
+from cx_Freeze import setup, Executable
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"  
+
+options = {
+    "build_exe": {
+        "packages": ["tkinter", "keyboard", "pyautogui"],
+        "include_files": [],
+    }
+}
+
+executables = [
+    Executable("Desstop.py", base=base)
+]
+
+setup(
+    name="Desstop",
+    version="0.1",
+    description="An ASCII faced desktop assistant",
+    options=options,
+    executables=executables
+)
