f675d97174ac6811c2134dafb8ceafa74f4f85a3
levi-ivel <levi.kingma@gmail.com>
2026-03-11 16:40:57 +0100

    Alter window content and positioning

--- Desstop/src-tauri/src/lib.rs
+++ Desstop/src-tauri/src/lib.rs
@@ -1,14 +1,29 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_positioner::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            use tauri::Manager;
+            use tauri_plugin_positioner::{Position, WindowExt};
+
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.set_position(tauri::LogicalPosition::new(0.0, 0.0));
+
+                let window_clone = window.clone();
+                std::thread::spawn(move || {
+                    std::thread::sleep(std::time::Duration::from_millis(1000));
+                    
+                    let _ = window_clone.set_position(tauri::LogicalPosition::new(0.0, 0.0));
+                    
+                    if let Ok(Some(_)) = window_clone.current_monitor() {
+                        let _ = window_clone.move_window(Position::TopLeft);
+                    }
+                });
+            }
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- README.md
+++ README.md
@@ -1,11 +1,24 @@
  Introduction
 ---
-Desstop is a simple spaces based notes app made in Rust using Tauri and SvelteKit.
-
+Desstop is a simple workspace based notes app made with Tauri, SvelteKit and Bun.
 
 
 Contributing
 ---
 This project is open source, so feel free to add or remove whatever you like. You can also make a pull request so your changes can be added to Desstop itself, documentation on how to create a pull request can be found [here](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/creating-a-pull-request)
 
-This project is still being worked on and is in a very early stage, expect frequent changes
+
+Disclaimer
+---
+This project is still being worked on and is in a very early stage, expect frequent changes.
+
+Some window managers may not accept the window settings, such as always on top and the positioning.
+
+One example of this is Hyprland, you can minigate this by adding this to your hyprconfig:
+```
+# Modern Hyprland syntax (Unified Rule System)
+windowrule = float 1, match:class ^(desstop)$
+windowrule = move 0 0, match:class ^(desstop)$
+windowrule = pin 1, match:class ^(desstop)$
+windowrule = no_initial_focus 1, match:class ^(desstop)$
+```
